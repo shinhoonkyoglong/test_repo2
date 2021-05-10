37e95dfc4b1f600355a2659292ca8eb60b774470
ghkdqhrkdus <83578751+ghkdqhrkdus@users.noreply.github.com>
2021-05-10 23:49:08 +0900

    Update gyo.py

--- gyo.py
+++ gyo.py
@@ -34,3 +34,24 @@ plt.xticks(x,month,rotation='vertical')
 plt.suptitle('Average frequency of typhoons per month over 10 years')
 
 plt.show()
+
+html = urlopen('https://www.weather.go.kr/w/eqk-vol/archive/stat/trend.do')
+soup = BeautifulSoup(html, 'lxml')
+
+namelist=soup.find_all('div',{'class':'over-scroll eqk-trend-content'})
+count=0
+for i in namelist:
+    count=count+1
+    if count>1:
+        a = i.get_text().split()
+LIST = [a[-55],a[-49],a[-43],a[-37],a[-31],a[-25],a[-19],a[-13],a[-7],a[-1]]
+print(LIST)
+x=[1,2,3,4,5,6,7,8,9,10]
+year=["2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
+plt.plot(x,LIST,label='me',color='b')
+plt.ylabel('count')
+plt.xlabel('year')
+plt.xticks(x,year,rotation='horizontal')
+plt.suptitle('frequency of earthquakes per year over 10 years')
+
+plt.show()
