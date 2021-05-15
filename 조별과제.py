5e0d3215d43db582caf5bd3a664eb42033db1211
ghkdqhrkdus <83578751+ghkdqhrkdus@users.noreply.github.com>
2021-05-15 21:55:35 +0900

    Update 조별과제.py

--- 조별과제.py
+++ 조별과제.py
@@ -1,34 +1,42 @@
 import time
 import selenium
 from selenium import webdriver
+import matplotlib.pyplot as plt
 
 URL = 'https://www.atfis.or.kr/sales/M002020000/search.do?salesTopItem=CD00000549&searchItem=CD00000573&searchDivision=CD00000600&searchYear=&searchQuarter=&x=61&y=35'
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
-driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')#, options=options)
 driver.get(url=URL)
 
-for name in range(4):
-    keyword_list_div = driver.find_elements_by_class_name("")
-    print(len(keyword_list_div))
+ramyeon_list=['신라면', '진라면', '너구리', '육개장']
 
-    date = keyword_list_div[0].find_element_by_class_name("no").text
-    print("--", f"date : {date}", "--")
+for i in range(4):
+    search_box=driver.find_element_by_class_name("chk_list")
+    search_btn.click("searchQuarter"+i)
 
-    prev_button = keyword_list_div[0].find_element_by_class_name("pre")
+    search_btn=driver.find_element_by_class_name("db_food_price_btns")
+    search_btn.click("검색")
 
-    keyword_list_div_row_list = keyword_list_div[0].find_elements_by_class_name("keywd_rank")
-    print(len(keyword_list_div_row_list))
-    for keyword_list_div_row in keyword_list_div_row_list:
-        rank = keyword_list_div_row.find_element_by_class_name("rank").text
-        keyword = keyword_list_div_row.find_element_by_class_name("keywd").text
-        print("\t", rank + " - " + keyword)
-    print("----")
-    print("")
+    ramyeon_table=driver.find_elements_by_class_name("table_view2")
+    ramyeon_table_tbody=ramyeon_table[0].find_elements_by_tag_name("tbody")
+    ramyeon_table_tbody_tr_list = ramyeon_table_tbody[0].find_elements_by_tag_name("tr")
 
-    prev_button.click()
+    ramyeon_box = list(ramyeon_table_tbody_tr_list)
+    count = 0
+    for ramyeon in ramyeon_list:
+        good = ramyeon_box.index(ramyeon)
+        money = (ramyeon_box[good + 1])
+        print(ramyeon, '=', money)
+        count = count + 1
+        x = count
+        y = money
+        plt.bar(x, y)
+        plt.xticks(x, ramyeon_list, rotation='horizontal')
+        plt.ylabel('단위=백만원')
+        plt.subtitle(i, '분기 각 라면 매출')
+
     time.sleep(3)
 
-time.sleep(3)
 
 driver.close()
